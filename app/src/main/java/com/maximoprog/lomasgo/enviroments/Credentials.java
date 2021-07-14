ab5c4251e4ec2b5eddc95450649ac86548513410
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-07-14 01:44:21 -0500

    ADD: diseño de info

--- app/src/main/java/com/maximoprog/lomasgo/enviroments/Credentials.java
+++ app/src/main/java/com/maximoprog/lomasgo/enviroments/Credentials.java
@@ -2,7 +2,7 @@ package com.maximoprog.lomasgo.enviroments;
 
 public final class Credentials {
 //    browser-sync start --proxy "http://localhost:1337" --files "css/*.css"
-    public static final String URL_API  = "http://192.168.129.240:3000/";
+    public static final String URL_API  = "http://192.168.232.240:3000";
     public static final String URI_NEWS  = "news";
     public static final String URI_SERVICES  = "services";
     public static final String URI_INFO  = "info";
